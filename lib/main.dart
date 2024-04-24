d4b5b90b987cb34e16757b49a70b35c6be53a3f9
Rowan Ibrahem <rowanibrahem2003@gmail.com>
2024-04-24 23:20:19 +0200

    solve payment error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import "package:afro_app/core/utilies/api_keys.dart";
 import "package:afro_app/features/payment/presentation/views/cart_view.dart";
-import "package:afro_app/features/splash_screen/presentation/views/splash_view.dart";
 import "package:device_preview/device_preview.dart";
 import "package:flutter/material.dart";
 import 'package:flutter_stripe/flutter_stripe.dart';
